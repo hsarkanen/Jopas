e6eb0421fbd0bac227a178bfa63e77025145725b
Heikki Sarkanen <heikki.sarkanen@gmail.com>
2014-08-02 02:04:26 +0300

    Buses w/o directionRef defined are actually in service

--- qml/js/sirilive.js
+++ qml/js/sirilive.js
@@ -123,12 +123,10 @@ LiveResult.prototype.parse_json = function(vehicles, parent) {
         var color = "#08a7cc"
         var vehicleTypeAndCode = {};
         if (parent.api_type !== 'helsinki') {
-            // No JORE codes or not used vehicles with "null" DirectionRef in use outside of Helsinki
+            // No JORE codes in use outside of Helsinki
             vehicleTypeAndCode = {"type": "bus", "code": code}
         }
         else {
-            if (vehicleData.MonitoredVehicleJourney.DirectionRef !== null)
-            {
                 // Jore parsing applied from example linked in: http://dev.hsl.fi/
                 if (code.match("^1019")) {code = "Ferry"; color = "#0080c8"; vehicleTypeAndCode = {"type": "ferry", "code": "Ferry"} /*Ferry*/}
                 else if (code.match(/^1300/)) {code = code.substring(4,5); color = "#ee5400"; vehicleTypeAndCode = {"type": "metro", "code": code} /*Metro*/}
@@ -136,11 +134,6 @@ LiveResult.prototype.parse_json = function(vehicles, parent) {
                 else if (code.match(/^10(0|10)/)) {code = code.substring(2,5).trim().replace(/^[0]?/,""); color = "#925bc6"; vehicleTypeAndCode = {"type": "tram", "code": code} /*Tram*/}
                 else if (code.match(/^(1|2|4).../)) {code = code.substring(1).replace(/^[0]?/,""); vehicleTypeAndCode = {"type": "bus", "code": code} /*Use default color for bus*/}
                 //else { console.debug("Unknown vehicle found.") /* Unknown, do nothing*/ }
-            }
-            else
-            {
-                continue  // No need to add vehicles which are not in service
-            }
         }
         // Show only vehicles included in the route
         var allowedVehicles = parent.model.vehicleCodesToShowOnMap
